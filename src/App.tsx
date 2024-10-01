f2c4b49b4d16fd23bddb3bbdc1535d0022866f79
Camilejo <camiloalejocc@gmail.com>
2024-10-01 10:44:17 -0500

    Arreglando problemas con los estilos de tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1>Hola</h1>
+      <div className="container">
+        <h1 className="text-4xl font-bold text-red-500 text-center m-5">
+          HOMEFINDER
+        </h1>
+      </div>
     </>
   )
 }
