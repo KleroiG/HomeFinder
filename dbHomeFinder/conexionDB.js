dce7bd1d7349f888280399859803dc4010cf0de2
Kevin Gomez <gomezk901@gmail.com>
2024-11-27 14:53:00 -0500

    Solucion compatibilidad 5d

--- dbHomeFinder/conexionDB.js
+++ dbHomeFinder/conexionDB.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import cors from 'cors'; // Middleware para permitir peticiones desde el frontend
-import { pool } from './database.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,11 +8,13 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 4173;
 
-// Ajusta la ruta para servir archivos estáticos desde la carpeta 'dist'
-app.use(express.static(path.join(__dirname, 'dist')));
+// Servir archivos estáticos desde la carpeta 'dist'
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 
+// Rutas para manejar cualquier solicitud y devolver el archivo index.html
+// Esto es para asegurarse de que todas las rutas se manejen por React Router
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
