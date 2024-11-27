d5020bee875a7646788ac744b8a6cf765373ea02
Kevin Gomez <gomezk901@gmail.com>
2024-11-27 14:10:07 -0500

    Solucion compatibilidad 5

--- dbHomeFinder/conexionDB.js
+++ dbHomeFinder/conexionDB.js
@@ -10,12 +10,12 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 4173;
 
-// Ajusta la ruta para servir archivos estáticos desde la carpeta correcta
-app.use(express.static(path.join(__dirname, '..', '..')));
+// Ajusta la ruta para servir archivos estáticos desde la raíz del proyecto
+app.use(express.static(path.join(__dirname, '..', '..', '..')));
 
 app.get('*', (req, res) => {
-  // Ajusta la ruta para enviar el archivo index.html desde la ubicación correcta
-  res.sendFile(path.join(__dirname, '..', '..', 'index.html'));
+  // Ajusta la ruta para enviar el archivo index.html desde la raíz del proyecto
+  res.sendFile(path.join(__dirname, '..', '..', '..', 'index.html'));
 });
 
 app.listen(PORT, () => {
